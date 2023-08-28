c929d374952afda3089e3192de6cc9bf4147362a
Stanislav Simunec <stanislavsimunec@gmail.com>
2023-08-29 01:27:49 +0200

    Initial commit

---
+++ backend/Readme.md
@@ -0,0 +1,19 @@
+# Introduction
+
+Test project to detect humans on provided image
+
+# Requirements
+
+1. Python 3.8.13
+2. PIP
+3. Python venv
+
+# Intallation
+
+Create virtual python environment
+
+Install packages using command:
+pip install -r requirements.txt
+
+# Running
+python app.py
---
+++ backend/detector.py
@@ -0,0 +1,67 @@
+from math import floor
+from transformers import YolosImageProcessor, YolosForObjectDetection
+from PIL import Image
+import torch
+import requests
+
+class PersonDetector:
+    def __init__(self):
+        self.model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')   
+        self.image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
+
+    def run_inference(self, image):
+        inputs = self.image_processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+
+        logits = outputs.logits
+        bboxes = outputs.pred_boxes
+
+        target_sizes = torch.tensor([image.size[::-1]])
+
+        results = self.image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+
+        data =  list(zip(results["scores"], results["labels"], results["boxes"]))
+
+        return data 
+
+    def map_inference_result(self, item):
+        score, label, box = item
+        box = [round(i, 2) for i in box.tolist()]
+        score = round(score.item(), 3)
+        label = self.model.config.id2label[label.item()]
+        size_p =  floor(abs(box[0] - box[2])  * abs(box[1] - box[3]))
+        return label, box, score, size_p
+    
+    def crop_image(self, inf_result_vec, source_image_path):
+        source_image = Image.open(source_image_path)
+
+        cropped_image = source_image.crop((inf_result_vec[1][0], 
+                            inf_result_vec[1][1], 
+                            inf_result_vec[1][2], 
+                            inf_result_vec[1][3]))
+        return cropped_image
+    
+    def take_size(self, element):
+        return element[len(element)-1]
+
+    def take_people(self, element):
+        return element[0] == 'person'
+
+    def run(self, source_image_path):
+        source_image = Image.open(source_image_path)
+
+        inference_result_raw = self.run_inference(source_image)
+
+        inference_result = list(map(self.map_inference_result, inference_result_raw))
+
+        inference_result = filter(self.take_people, inference_result)
+
+        inference_result = sorted(inference_result, key=self.take_size, reverse=True)
+
+        return inference_result
+    
+    def find_closest(self, inference_result):
+        inference_result = sorted(inference_result, key=self.take_size, reverse=True)
+        return next(iter(inference_result or []), None)
+
+
