51d6976b46360d72d469610b71ba4d68cc772cf4
Stanislav <stanislavsimunec@gmail.com>
2023-08-29 02:15:01 +0200

    added deepface

--- backend/console.py
+++ backend/console.py
@@ -1,59 +1,25 @@
-from math import floor
-from transformers import YolosImageProcessor, YolosForObjectDetection
-from PIL import Image
-import torch
-import requests
-import detector
-import os
-from fastai.vision.all import *
-from PIL import Image
+
 from deepface import DeepFace
 
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"]=""
 
-import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+objs = DeepFace.analyze(img_path = "person1.jpg", 
+        actions = ['age','gender','emotion']
+)
 
-source_image_path = './person1.jpg'
+print(objs)
 
-# person_detector = detector.PersonDetector()
+print(objs[0]["age"])
+print(objs[0]["region"]["x"])
 
-# inference_result = person_detector.run(source_image_path)
+genders = objs[0]["gender"]
+gender = max(genders, key=genders.get).lower()
+print(gender)
 
-# print(inference_result)
+emotions = objs[0]["emotion"]
+emotion = max(emotions, key=emotions.get)
+print(emotion)
 
-# closest = person_detector.find_closest(inference_result)
-
-# print(closest)
-# result_image = person_detector.crop_image(closest, source_image_path)
-
-# result_image.show()
-
-# Emotion
-
-learn_emotion = load_learner('./emotions_vgg19.pkl')
-learn_emotion_labels = learn_emotion.dls.vocab
-
-# Sentiment
-learn_sentiment = load_learner('./sentiment_vgg19.pkl')
-learn_sentiment_labels = learn_sentiment.dls.vocab
-
-def predict(img):
-    img = PILImage.create(img)
-    
-    pred_emotion, pred_emotion_idx, probs_emotion = learn_emotion.predict(img)
-    
-    pred_sentiment, pred_sentiment_idx, probs_sentiment = learn_sentiment.predict(img)
-    
-    #emotions = {f'emotion_{learn_emotion_labels[i]}': float(probs_emotion[i]) for i in range(len(learn_emotion_labels))}
-    #sentiments = {f'sentiment_{learn_sentiment_labels[i]}': float(probs_sentiment[i]) for i in range(len(learn_sentiment_labels))}
-    
-    emotions = {learn_emotion_labels[i]: float(probs_emotion[i]) for i in range(len(learn_emotion_labels))}
-
-    print(emotions)
-    sentiments = {learn_sentiment_labels[i]: float(probs_sentiment[i]) for i in range(len(learn_sentiment_labels))}
-        
-    print(sentiments)
-    return [emotions, sentiments] #{**emotions, **sentiments}
-
-result = predict(source_image_path)
+possible_emotions = list(emotions)
+print(possible_emotions)
